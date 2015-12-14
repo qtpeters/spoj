b37ebb5fe28fe07194916253c5cefff9c8a6c87d
qtpeters <qtpeters>
2015-12-13 20:06:54 -0500

    Starting new problem

---
+++ hr.evaluating.ex/src/main/java/hr/eval/ex/Solution.java
@@ -0,0 +1,15 @@
+package hr.eval.ex;
+
+import java.util.Scanner;
+
+public class Solution {
+   public static void main(String[] args) {
+        
+        Scanner s = new Scanner( System.in );
+        int testCases = s.nextInt();
+        for ( int i=0; i<testCases; i++ ) {
+        }
+
+        s.close();
+    }
+}
