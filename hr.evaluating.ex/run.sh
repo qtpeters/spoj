552ef6c95c80937a16bb495200d5558a5ace1946
qtpeter <quinn.peters@nissint.com>
2015-12-15 07:52:34 -0500

    Really close except I have rounding errors

--- hr.evaluating.ex/run.sh
+++ hr.evaluating.ex/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export GRADLE_OPTS=-Dorg.gradle.daemon=true
+#export GRADLE_OPTS=-Dorg.gradle.daemon=true
 gradle clean build;
 echo "********** OUTPUT BELOW **********"
-scala -cp build/libs/hr.evaluating.ex.jar hr.eval.ex.Solution;
+cat test.txt | scala -cp build/libs/hr.evaluating.ex.jar hr.eval.ex.Solution;
 echo "**********  END OUTPUT  **********"
--- hr.evaluating.ex/src/main/scala/hr/eval/ex/Solution.scala
+++ hr.evaluating.ex/src/main/scala/hr/eval/ex/Solution.scala
@@ -1,5 +1,39 @@
 package hr.eval.ex;
 
+import scala.io.StdIn;
+
 object Solution extends App {
-	println( "Testing" )
+
+	def r( value:Double ) : Double = {
+		BigDecimal( value ).setScale( 
+			4, BigDecimal.RoundingMode.HALF_UP 
+		).toDouble
+	}
+
+	def p( value:Double, pow:Int ) : Double = {
+		if ( pow > 1 )
+			return value * p( value, pow - 1 )
+		else value
+	}
+
+	def f( num:Int ) : Double = {
+		if ( num > 0 ) 
+			return num * f( num - 1 ) 
+		else 1
+	}
+
+	def evalFor( value:Double, itr:Int=0 ) : Double = {
+		if ( itr < 10 )
+			p( value, itr )/f( itr ) + evalFor( value, itr + 1 )
+		else 0.0
+	} 
+	
+	def exeTestCase( tn:Int ) : Unit = {
+		if ( tn > 0 ) {
+			exeTestCase( tn - 1 );
+			println( evalFor( scala.io.StdIn.readDouble() ) )
+		}
+	}
+	
+	exeTestCase( scala.io.StdIn.readInt() )
 }
