b9fa61c5019c142178b2c682cf9fa1242baca29a
qtpeters <qtpeters>
2015-12-26 17:28:53 -0500

    Commit to switch branches

--- hr.no.prefix.set/src/main/java/hr/no/prefix/set/Solution.java
+++ hr.no.prefix.set/src/main/java/hr/no/prefix/set/Solution.java
@@ -1,20 +1,27 @@
 package hr.no.prefix.set;
 
 import java.util.*;
+import java.io.*;
 
 public class Solution {
 
    public static List<String> strings = new ArrayList<String>();
    public static int numStrings;
    
-   private static String process() {
-      Scanner s = new Scanner( System.in );
-      int numStrings = s.nextInt();
+   private static String process() throws Exception {
 
+      // Create a scanner to read from STDIN
+      BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
+
+      // Read in the number of strings 
+      int numStrings = Integer.valueOf( br.readLine() );
+
+      // Iterate 'numStrings' times:
+      // * Get a 
       String foundPrefixString = null;
       for ( int i=0; i<numStrings; i++ ) {
          
-         String next = s.next();
+         String next = br.readLine();
          for ( String string : strings ) {
             if ( string.startsWith( next ) || next.startsWith( string ) ) { 
                foundPrefixString = next;
@@ -29,7 +36,7 @@ public class Solution {
       return foundPrefixString;
    }
 
-   public static void main( String [] args ) {
+   public static void main( String [] args ) throws Exception {
       String str = null;
       if ( ( str = process() ) != null ) {
          System.out.println( "BAD SET" );
