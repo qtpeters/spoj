2587b619697b3eb41c99244881550e0a0191c9f7
qtpeter <quinn.peters@nissint.com>
2015-12-29 11:22:38 -0500

    Added file with URL

--- hr.cube.summation/run.sh
+++ hr.cube.summation/run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Cube Summation: https://www.hackerrank.com/challenges/cube-summation
 pkg=$( basename ${PWD} )
 gradle clean build
 cat test.txt | java -cp build/libs/${pkg}.jar ${pkg}.Solution
